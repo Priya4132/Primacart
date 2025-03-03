8d13a1e5f77d0f24024692863aadf0fc9f86a918
Priya Tripathi <priya.aakashmishra@gmail.com>
2025-03-03 22:23:58 +0530

    Update baseUrl.js

--- scripts/baseUrl.js
+++ scripts/baseUrl.js
@@ -1,2 +1,5 @@
 // export const baseUrl = "http://localhost:3000";
-export const baseUrl = "https://polarized-concrete-desert.glitch.me";
+//export const baseUrl = "https://polarized-concrete-desert.glitch.me";
+export const baseUrl= "https://ember-bronze-aragosaurus.glitch.me"
+
+//https://github.com/Priya4132/deployJsonUsingGlitch
